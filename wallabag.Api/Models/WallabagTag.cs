2c0a0d7c957faf6a0f961a477185adaaf563121f
Julian Oster <github@jlnostr.de>
2016-05-13 13:12:45 +0200

    Add slug property to WallabagTag.

--- wallabag.Api/Models/WallabagTag.cs
+++ wallabag.Api/Models/WallabagTag.cs
@@ -16,6 +16,12 @@ namespace wallabag.Api.Models
         [JsonProperty("label")]
         public string Label { get; set; }
 
+        /// <summary>
+        /// Gets or sets the slug of the tag.
+        /// </summary>
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
         public override string ToString() => this.Label;
         public override int GetHashCode() => this.Id;
         public override bool Equals(object obj) => (obj as WallabagTag).Id.Equals(this.Id);
