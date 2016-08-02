3b6e6272982c20d96f7853c6f451ae6ff25768fb
jlnostr <jlnostr@DESKTOP-UTAHR2R>
2016-08-02 20:11:52 +0200

    Add ExceptionIsThrownWhenTimeoutIsReached test.

---
+++ wallabag.Api.Tests/ExceptionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace wallabag.Api.Tests
+{
+    public partial class GeneralTests
+    {
+        [TestMethod]
+        [TestCategory("Exceptions")]
+        public async Task ExceptionIsThrownWhenTimeoutIsReached()
+        {
+            var previousTimeout = client.Timeout;
+            client.Timeout = 5;
+            
+            await AssertExtensions.ThrowsExceptionAsync<TimeoutException>(async () => await client.RefreshAccessTokenAsync());
+
+            client.Timeout = previousTimeout;
+        }
+    }
+}
