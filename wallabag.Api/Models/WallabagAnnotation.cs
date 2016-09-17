d9c4233ce30bdd1b728084bdc9c20f154d69b388
Julian Oster <jlnostr@outlook.de>
2016-09-17 12:49:20 +0200

    Updater annotation base class.

--- wallabag.Api/Models/WallabagAnnotation.cs
+++ wallabag.Api/Models/WallabagAnnotation.cs
@@ -1,4 +1,5 @@
 ﻿using PropertyChanged;
+using System;
 
 namespace wallabag.Api.Models
 {
@@ -6,6 +7,20 @@ namespace wallabag.Api.Models
     public class WallabagAnnotation
     {
         public int Id { get; set; }
+        public string AnnotatorSchemaVersion { get; set; }
+        public string Quote { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Text { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public WallabagAnnotationRange Range { get; set; }
+    }
 
+    [ImplementPropertyChanged]
+    public class WallabagAnnotationRange
+    {
+        public string Start { get; set; }
+        public int StartOffset { get; set; }
+        public string End { get; set; }
+        public int EndOffset { get; set; }
     }
 }
