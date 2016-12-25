8e03ecc1826eadf112e233c39ef9ea2d676ae24f
Julian Oster <jlnostr@outlook.de>
2016-12-25 23:35:10 +0100

    Add implementation for annotations.

--- wallabag.Api/Components/Annotations.cs
+++ wallabag.Api/Components/Annotations.cs
@@ -1,25 +1,63 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Net.Http;
-using wallabag.Api.Responses;
 using wallabag.Api.Models;
 
 namespace wallabag.Api
 {
     public partial class WallabagClient
     {
-        public Task<IEnumerable<WallabagAnnotation>> GetAnnotationsAsync(WallabagItem item) => GetAnnotationsAsync(item.Id);
-        public async Task<IEnumerable<WallabagAnnotation>> GetAnnotationsAsync(int itemId) => null;
+        public Task<IEnumerable<WallabagAnnotation>> GetAnnotationsAsync(WallabagItem item, CancellationToken cancellationToken = default(CancellationToken))
+            => GetAnnotationsAsync(item.Id, cancellationToken);
+        public async Task<IEnumerable<WallabagAnnotation>> GetAnnotationsAsync(int itemId, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            string jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Get, $"/annotations/{itemId}", cancellationToken);
+            var result = await ParseJsonFromStringAsync<WallabagAnnotationRoot>(jsonString, cancellationToken);
 
-        public Task<bool> AddAnnotationAsync(WallabagItem item, WallabagAnnotation annotation) => AddAnnotationAsync(item.Id, annotation);
-        public async Task<bool> AddAnnotationAsync(int itemId, WallabagAnnotation annotation) => false;
+            return result.Annotations ?? new List<WallabagAnnotation>();
+        }
 
-        public Task<bool> UpdateAnnotationAsync(WallabagAnnotation oldAnnotation, WallabagAnnotation newAnnotation) => UpdateAnnotationAsync(oldAnnotation.Id, newAnnotation);
-        public async Task<bool> UpdateAnnotationAsync(int oldAnnotationId, WallabagAnnotation newAnnotation) => false;
+        public Task<WallabagAnnotation> AddAnnotationAsync(WallabagItem item, WallabagAnnotation annotation, CancellationToken cancellationToken = default(CancellationToken))
+            => AddAnnotationAsync(item.Id, annotation, cancellationToken);
+        public async Task<WallabagAnnotation> AddAnnotationAsync(int itemId, WallabagAnnotation annotation, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { "ranges", annotation.Ranges },
+                { "text", annotation.Text }
+            };
 
-        public Task<bool> DeleteAnnotationAsync(WallabagAnnotation annotation) => DeleteAnnotationAsync(annotation.Id);
-        public async Task<bool> DeleteAnnotationAsync(int annotationId) => false;
+            if (!string.IsNullOrEmpty(annotation.Quote))
+                parameters.Add("quote", annotation.Quote);
+
+            string jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Post, $"/annotations/{itemId}", cancellationToken, parameters);
+            var result = await ParseJsonFromStringAsync<WallabagAnnotation>(jsonString, cancellationToken);
+
+            return result;
+        }
+
+        public Task<WallabagAnnotation> UpdateAnnotationAsync(WallabagAnnotation oldAnnotation, WallabagAnnotation newAnnotation, CancellationToken cancellationToken = default(CancellationToken))
+            => UpdateAnnotationAsync(oldAnnotation.Id, newAnnotation, cancellationToken);
+        public async Task<WallabagAnnotation> UpdateAnnotationAsync(int oldAnnotationId, WallabagAnnotation newAnnotation, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                { "ranges", newAnnotation.Ranges },
+                { "text", newAnnotation.Text }
+            };
+
+            if (!string.IsNullOrEmpty(newAnnotation.Quote))
+                parameters.Add("quote", newAnnotation.Quote);
+
+            string jsonString = await ExecuteHttpRequestAsync(HttpRequestMethod.Put, $"/annotations/{oldAnnotationId}", cancellationToken, parameters);
+            var result = await ParseJsonFromStringAsync<WallabagAnnotation>(jsonString, cancellationToken);
+
+            return result;
+        }
+
+        public Task<bool> DeleteAnnotationAsync(WallabagAnnotation annotation, CancellationToken cancellationToken = default(CancellationToken))
+            => DeleteAnnotationAsync(annotation.Id, cancellationToken);
+        public async Task<bool> DeleteAnnotationAsync(int annotationId, CancellationToken cancellationToken = default(CancellationToken))
+            => !string.IsNullOrEmpty(await ExecuteHttpRequestAsync(HttpRequestMethod.Delete, $"/annotations/{annotationId}", cancellationToken));
     }
 }
