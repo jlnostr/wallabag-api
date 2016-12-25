2a9eb6b42eb48971ffa6172e1e4c56da92d7e3ba
Julian Oster <jlnostr@outlook.de>
2016-12-25 18:56:07 +0100

    Update WallabagAnnotation.cs with latest API changes.

--- wallabag.Api/Models/WallabagAnnotation.cs
+++ wallabag.Api/Models/WallabagAnnotation.cs
@@ -1,26 +1,59 @@
-﻿using PropertyChanged;
+﻿using Newtonsoft.Json;
+using PropertyChanged;
 using System;
+using System.Collections.Generic;
 
 namespace wallabag.Api.Models
 {
     [ImplementPropertyChanged]
+    internal class WallabagAnnotationRoot
+    {
+        [JsonProperty("total")]
+        public int Count { get; set; }
+
+        [JsonProperty("rows")]
+        public IEnumerable<WallabagAnnotation> Annotations { get; set; }
+    }
+
+    [ImplementPropertyChanged]
     public class WallabagAnnotation
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("annotator_schema_version")]
         public string AnnotatorSchemaVersion { get; set; }
+
+        [JsonProperty("quote")]
         public string Quote { get; set; }
+
+        [JsonProperty("created_at")]
         public DateTime CreationDate { get; set; }
+
+        [JsonProperty("text")]
         public string Text { get; set; }
+
+        [JsonProperty("updated_at")]
         public DateTime LastUpdated { get; set; }
-        public WallabagAnnotationRange Range { get; set; }
+
+        [JsonProperty("ranges")]
+        public IEnumerable<WallabagAnnotationRange> Ranges { get; set; }
     }
 
     [ImplementPropertyChanged]
     public class WallabagAnnotationRange
     {
+        [JsonProperty("start")]
         public string Start { get; set; }
+
+        [JsonProperty("startOffset")]
         public int StartOffset { get; set; }
+
+        [JsonProperty("end")]
         public string End { get; set; }
+
+        [JsonProperty("endOffset")]
         public int EndOffset { get; set; }
+
     }
 }
